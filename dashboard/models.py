08cfa7f00be5b774da7e413985838e0abcd70031
Sunny-Creature <soniaisissoninha119@gmail.com>
2025-12-09 15:13:13 -0300

    adicionando mais algumas coisas e começando dashboard

--- dashboard/models.py
+++ dashboard/models.py
@@ -5,3 +5,4 @@ class UserLogin(models.Model):
     user_name = models.CharField(max_length=20)
     user_email = models.EmailField(null=True)
     user_password = models.CharField(max_length=10)
+
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, permission_required
+from nuarte.models import Grupos, Orientadores, Eventos, Historia
 from dashboard.models import UserLogin
 
 # Create your views here.
+
+@login_required
 def dashboard(request):
     context = {
         "user_login": UserLogin.objects.all(),
