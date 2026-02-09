872c9eb15024893e7621ede35dbfb5f6352d8844
Sunny-Creature <soniaisissoninha119@gmail.com>
2026-02-09 09:35:00 -0300

    dificil lidar sozinha e de ultima hora

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import permission_required, login_required
+from nuarte.models import Coordenadores, Grupos, Eventos, Historia
+from usuarios.models import Usuario
 
 # Create your views here.
 
+#@login_required
 #@permission_required
-#def index(request):
-#    context = {
-
-#    }
